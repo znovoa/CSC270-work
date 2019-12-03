0fe567e31d5fcf3cb400482a48e15a7b14e936e3
Zoe Novoa <zoe.novoa@furman.edu>
2019-12-03 18:55:48 +0000

    commiting untracked files

--- src/main/scala/web-writer.scala
+++ src/main/scala/web-writer.scala
@@ -34,7 +34,7 @@ object webWriter {
   			}
 	  		case None => // do nothing
   		}
-  		
+
   }
 
 
@@ -60,12 +60,12 @@ object webWriter {
 				/* -------------------------------------- */
 				// 2. Split it into manageable chunks. THERE ARE SEVERAL WAYS TO DO THIS!
 
-				//val corpVec: Vector[Corpus] = hocuspocus.corpusToChapters( corp, drop = 1 )
-				val corpVec: Vector[Corpus] = hocuspocus.equalDivs( corp, n = 10 )
+				val corpVec: Vector[Corpus] = hocuspocus.corpusToChapters( corp, drop = 1 )
+			  //val corpVec: Vector[Corpus] = hocuspocus.equalDivs( corp, n = 10 )
 				//val corpVec: Vector[Corpus] = hocuspocus.equalSize( corp, target = 5000 )
 				/* -------------------------------------- */
 
-				// A little reporting… 	
+				// A little reporting…
 				println(" \n ")
 				println(s"Will write ${corpVec.size} pages…")
 				println(" \n ")
@@ -80,7 +80,7 @@ object webWriter {
 					hv.save( filePath = htmlDirectory)
 				}
 
-				// 5. Write the landing page, with the toc.	
+				// 5. Write the landing page, with the toc.
 				landingPage(htmlVec)
 
 		} catch {
@@ -91,7 +91,7 @@ object webWriter {
   }
 
 
-  /* 
+  /*
 				A function: Given a URN and a number, generate a file-name.
   */
   def urnToFileName( urn: CtsUrn, index: Option[Int] = None ): String = {
@@ -106,11 +106,11 @@ object webWriter {
   	}
   }
 
-  /* 
-			We define a new object or "class", HtmlCorpus, that has all the 
+  /*
+			We define a new object or "class", HtmlCorpus, that has all the
 			data we need to write HTML pages for each chunk.
 
-			This class has a .toString method, and a .html method that does 
+			This class has a .toString method, and a .html method that does
 			our work for us.
   */
 	case class HtmlCorpus(
@@ -134,7 +134,7 @@ object webWriter {
 
 
 		def html: String = {
-			val catString: String = catacomb.html(cat)	
+			val catString: String = catacomb.html(cat)
 			val corpString: String = hocuspocus.html(corp)
 
 			val titleString: String = {
@@ -250,11 +250,11 @@ object webWriter {
 
 		val workCat: CatalogEntry = vcorp.head.cat
 
-		val catString: String = catacomb.html(workCat)	
+		val catString: String = catacomb.html(workCat)
 
 		val titleString: String = workCat.toString // can fancify this
-	
-		val infoString: String = """<div class="cts_siteDesc">Whatever you want to say here.</div>"""		
+
+		val infoString: String = """<div class="cts_siteDesc">Whatever you want to say here.</div>"""
 
 		val tocHeader: String = """<div class="cts_toc">Table of Contents</div>"""
 
@@ -262,15 +262,15 @@ object webWriter {
 			val firstPassage: String = vc.corp.nodes.head.urn.passageComponent
 			val lastPassage: String = vc.corp.nodes.last.urn.passageComponent
 			if (firstPassage == lastPassage) {
-				s"""<li class="cts_tocEntry"><span class="cts_tocIndex">${vc.index + 1}.</span> 
+				s"""<li class="cts_tocEntry"><span class="cts_tocIndex">${vc.index + 1}.</span>
 				<a href="${urnToFileName(vc.corp.nodes.head.urn.dropPassage, Some(vc.index))}">
 				<span class="cts_tocBit">${firstPassage}</span>
 				</a></li>"""
 			} else {
-				s"""<li class="cts_tocEntry"><span class="cts_tocIndex">${vc.index + 1}.</span> 
+				s"""<li class="cts_tocEntry"><span class="cts_tocIndex">${vc.index + 1}.</span>
 				<a href="${urnToFileName(vc.corp.nodes.head.urn.dropPassage, Some(vc.index))}">
 				<span class="cts_tocBit">${firstPassage}</span><span class="cts_tocHyphen">–</span><span class="cts_tocBit">${lastPassage}</span>
-				</a></li>"""	
+				</a></li>"""
 			}
 		})
 
@@ -293,7 +293,7 @@ object webWriter {
 			${catString}
 			${infoString}
 			${tocHeader}
-			${toc}	
+			${toc}
 			</article>
 			<footer>Your footer</footer>
 			</body>
